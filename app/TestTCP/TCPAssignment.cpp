8abf7ce551edb44a97b7d5b058ce162faee293a2
thestarkor <sk_and_mc@kaist.ac.kr>
2020-08-13 22:22:54 +0900

    pass 6/8

--- app/TestTCP/TCPAssignment.cpp
+++ app/TestTCP/TCPAssignment.cpp
@@ -16,6 +16,7 @@
 
 namespace E
 {
+std::list<Socket*> socket_list;
 
 TCPAssignment::TCPAssignment(Host* host) : HostModule("TCP", host),
 		NetworkModule(this->getHostModuleName(), host->getNetworkSystem()),
@@ -33,7 +34,6 @@ TCPAssignment::~TCPAssignment()
 
 void TCPAssignment::initialize()
 {
-
 }
 
 void TCPAssignment::finalize()
@@ -46,10 +46,10 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid, const SystemCallPa
 	switch(param.syscallNumber)
 	{
 	case SOCKET:
-		//this->syscall_socket(syscallUUID, pid, param.param1_int, param.param2_int, param.param3_int);
+		this->syscall_socket(syscallUUID, pid, param.param1_int, param.param2_int, param.param3_int);
 		break;
 	case CLOSE:
-		//this->syscall_close(syscallUUID, pid, param.param1_int);
+		this->syscall_close(syscallUUID, pid, param.param1_int);
 		break;
 	case READ:
 		//this->syscall_read(syscallUUID, pid, param.param1_int, param.param2_ptr, param.param3_int);
@@ -70,9 +70,9 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid, const SystemCallPa
 		//		static_cast<socklen_t*>(param.param3_ptr));
 		break;
 	case BIND:
-		//this->syscall_bind(syscallUUID, pid, param.param1_int,
-		//		static_cast<struct sockaddr *>(param.param2_ptr),
-		//		(socklen_t) param.param3_int);
+		this->syscall_bind(syscallUUID, pid, param.param1_int,
+				static_cast<struct sockaddr *>(param.param2_ptr),
+				(socklen_t) param.param3_int);
 		break;
 	case GETSOCKNAME:
 		//this->syscall_getsockname(syscallUUID, pid, param.param1_int,
@@ -87,6 +87,62 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid, const SystemCallPa
 	default:
 		assert(0);
 	}
+}
+
+Socket* TCPAssignment::find_socket(int pid, int fd)
+{
+	Socket* socket = NULL;
+
+	for (std::list<Socket*>::iterator i = socket_list.begin(); i != socket_list.end(); ++i){
+		if (pid == (*i)->pid && fd == (*i)->fd) {
+			socket = *i;
+		}
+	}
+
+	return socket;
+}
+
+void TCPAssignment::syscall_socket(UUID syscallUUID, int pid, int domain, int type, int protocol)
+{
+	int fd = createFileDescriptor(pid);
+	Socket* socket = new Socket(pid, fd);
+	socket_list.push_back(socket);
+	returnSystemCall(syscallUUID, fd);
+}
+
+void TCPAssignment::syscall_close(UUID syscallUUID, int pid, int sockfd)
+{
+	for (std::list<Socket*>::iterator it = socket_list.begin(); it != socket_list.end(); ++it) {
+		if (pid == (*it)->pid && sockfd == (*it)->fd) 
+		{
+			free((*it)->sa);
+			socket_list.erase(it);
+			removeFileDescriptor(pid, sockfd);
+			returnSystemCall(syscallUUID, 0);
+			return;
+		}
+	}
+
+	returnSystemCall(syscallUUID, -1);
+}
+
+void TCPAssignment::syscall_bind(UUID syscallUUID, int pid, int sockfd, struct sockaddr *my_addr, socklen_t addrlen)
+{
+	Socket* socket = find_socket(pid, sockfd);
+
+	if (socket == NULL || socket->state != IDLE) {
+		returnSystemCall(syscallUUID, -1);
+		return;
+	}
+
+	struct sockaddr_in* new_sa = (struct sockaddr_in *) malloc(sizeof(struct sockaddr_in));
+	new_sa->sin_family = ((struct sockaddr_in *) my_addr)->sin_family;
+	new_sa->sin_addr = ((struct sockaddr_in *) my_addr)->sin_addr;
+	new_sa->sin_port = ((struct sockaddr_in *) my_addr)->sin_port;
+	socket->sa = (struct sockaddr *) new_sa;
+	socket->state = BOUND;
+
+	returnSystemCall(syscallUUID, 0);
 }
 
 void TCPAssignment::packetArrived(std::string fromModule, Packet* packet)
